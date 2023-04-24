fc944be58ab593e1d1c0d4237b17814cdcf4af66
Gurkirat-s <gurkiratsingh@hotmail.ca>
2023-04-24 13:41:32 -0400

    Fixed button not scrolling to position and added gap between text and icon in project buttons

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './Hero.css';
 import { HiArrowNarrowRight } from 'react-icons/hi';
+import { Link } from 'react-scroll';
 
 const Hero = () => {
   return (
@@ -9,12 +10,12 @@ const Hero = () => {
         <h2>Hi, my name is</h2>
         <h1>Gurkirat Singh</h1>
         <p>I'm a Software Developer from Brampton, Ontario.</p>
-        <button className="btn" href="#projects">
+        <Link className="btn" to="projects" spy={true} smooth={true}>
           See some projects I've worked on
           <span className="arrow">
             <HiArrowNarrowRight />
           </span>
-        </button>
+        </Link>
       </div>
     </div>
   );
